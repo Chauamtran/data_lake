72f0c0c141c0f6e5231fa18fc5965c12c79d6d0a
chauamtran <zaracattle@gmail.com>
2019-12-21 23:58:22 +0700

    Add pipeline logstash configs

--- hdfs_hive_spark/logstash/logstash.sh
+++ hdfs_hive_spark/logstash/logstash.sh
@@ -2,14 +2,18 @@
 # Author: Chau Tran
 
 VERSION='6.8.5'
-CONFIG=$1
+FOLDER_CONFIG=$1
+
+printf "Passing %s %s" "${FOLDER_CONFIG}" "${VERSION}"
 
 #UID=$(id -u)
 #GID=$(id -g)
 
 function buildServices {
   echo ">> Building logstash images ..."
-  docker build  --build-arg CONFIG="${CONFIG}.conf" --build-arg VERSION="${VERSION}" --rm -t logstash_"${CONFIG}":"${VERSION}" -f Dockerfile .
+  docker build --rm -t logstash_"${FOLDER_CONFIG}":"${VERSION}" -f Dockerfile \
+               --build-arg FOLDER="${FOLDER_CONFIG}" \
+               --build-arg VERSION="${VERSION}" .
   sleep 5
 }
 
@@ -18,32 +22,33 @@ function startServices {
   echo ">> Starting logstash ..."
   docker run -d --net zookeepernet \
               --ip 182.18.1.8 \
-              --hostname "${CONFIG}" \
-              --name "${CONFIG}" \
+              --hostname "${FOLDER_CONFIG}" \
+              --name "${FOLDER_CONFIG}" \
               --restart always \
               --link kafka-1:kafka-1 \
               --link kafka-2:kafka-2 \
               --link kafka-3:kafka-3 \
               --link nodemaster:nodemaster \
+              -v "${FOLDER_CONFIG}":/usr/share/logstash/"${FOLDER_CONFIG}" \
               -p 5044:5044 \
-              logstash_"${CONFIG}":"${VERSION}"
+              logstash_"${FOLDER_CONFIG}":"${VERSION}"
 
   echo ">> Connecting logstash to hadoops network interface"
-  docker network connect hadoopnet "${CONFIG}"
+  docker network connect hadoopnet "${FOLDER_CONFIG}"
 
   sleep 5
 }
 
 function stopServices {
   echo ">> Stopping logstash containers ..."
-  docker stop "${CONFIG}"
+  docker stop "${FOLDER_CONFIG}"
 }
 
 function deleteServices {
   echo ">> Stopping logstash containers ..."
-  docker stop "${CONFIG}"
+  docker stop "${FOLDER_CONFIG}"
   echo ">> Deleting logstash containers ..."
-  docker rm "${CONFIG}"
+  docker rm "${FOLDER_CONFIG}"
 }
 
 if [[ $2 = 'start' ]]; then
@@ -61,8 +66,9 @@ elif [[ $2 = 'build' ]]; then
   buildServices
 else
   echo "Usage: logstash.sh instance_name start|stop|delete"
-  echo "                 start - a logstash instance"
-  echo "                 stop - a logstash instance"
-  echo "                 delete - a logstash instance"
+  echo "                 instance_name - a folder where stores config of logstash instance"
+  echo "                 start - start logstash instance"
+  echo "                 stop - stop logstash instance"
+  echo "                 delete - delete logstash instance"
 fi
 
